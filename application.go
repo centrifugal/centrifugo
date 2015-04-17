31dd5ca1c0b3fd3e3e4b57ebe08e80ba3aac1d7d
FZambia <frvzmb@gmail.com>
2015-04-17 21:31:55 +0300

    move api command handling to its own file

--- application.go
+++ application.go
@@ -11,7 +11,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/mitchellh/mapstructure"
 	"github.com/nu7hatch/gouuid"
 	"github.com/spf13/viper"
 )
@@ -369,91 +368,4 @@ func (app *application) isUserAllowed(channel, user string) bool {
 		}
 	}
 	return false
-}
-
-func (app *application) handleApiCommand(p *project, command apiCommand) (*response, error) {
-
-	var err error
-	var resp *response
-
-	method := command.Method
-	params := command.Params
-
-	switch method {
-	case "publish":
-		resp, err = app.handlePublishCommand(p, params)
-	case "unsubscribe":
-		resp, err = app.handleUnsubscribeCommand(p, params)
-	case "disconnect":
-		resp, err = app.handleDisconnectCommand(p, params)
-	case "presence":
-		resp, err = app.handlePresenceCommand(p, params)
-	case "history":
-		resp, err = app.handleHistoryCommand(p, params)
-	default:
-		return nil, ErrMethodNotFound
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
-}
-
-func (app *application) handlePublishCommand(p *project, ps Params) (*response, error) {
-	resp := newResponse("publish")
-	var cmd publishApiCommand
-	err := mapstructure.Decode(ps, &cmd)
-	if err != nil {
-		return nil, ErrInvalidApiMessage
-	}
-	channel := cmd.Channel
-	data := cmd.Data
-	err = app.publishClientMessage(p, channel, data, nil)
-	if err != nil {
-		resp.Error = ErrInternalServerError
-	}
-	return resp, nil
-}
-
-func (app *application) handleUnsubscribeCommand(p *project, ps Params) (*response, error) {
-	return newResponse("unsubscribe"), nil
-}
-
-func (app *application) handleDisconnectCommand(p *project, ps Params) (*response, error) {
-	return newResponse("disconnect"), nil
-}
-
-func (app *application) handlePresenceCommand(p *project, ps Params) (*response, error) {
-	resp := newResponse("presence")
-	var cmd presenceApiCommand
-	err := mapstructure.Decode(ps, &cmd)
-	if err != nil {
-		return nil, ErrInvalidApiMessage
-	}
-	channel := cmd.Channel
-	presence, err := app.getPresence(p.Name, channel)
-	if err != nil {
-		resp.Error = ErrInternalServerError
-		return resp, nil
-	}
-	resp.Body = presence
-	return resp, nil
-}
-
-func (app *application) handleHistoryCommand(p *project, ps Params) (*response, error) {
-	resp := newResponse("history")
-	var cmd historyApiCommand
-	err := mapstructure.Decode(ps, &cmd)
-	if err != nil {
-		return nil, ErrInvalidApiMessage
-	}
-	channel := cmd.Channel
-	history, err := app.getHistory(p.Name, channel)
-	if err != nil {
-		resp.Error = ErrInternalServerError
-		return resp, nil
-	}
-	resp.Body = history
-	return resp, nil
 }
