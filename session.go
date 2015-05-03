b28565d601ea693180a16c424fe4acf11f7fbb6a
FZambia <frvzmb@gmail.com>
2015-05-03 15:00:22 +0300

    reorginize repo structure

--- session.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// Session represents a connection between server and client.
-type session interface {
-	// Send sends one text frame to session
-	Send(string) error
-	// Close closes the session with provided code and reason.
-	Close(status uint32, reason string) error
-}
