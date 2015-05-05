bec974be1f2b621d41754e7dcfabb7d28b71af5a
Alexandr Emelin <frvzmb@gmail.com>
2015-05-05 20:21:16 +0300

    add release.sh

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ "$1" = "" ]
+then
+  echo "Usage: $0 <version>"
+  exit
+fi
+
+mkdir -p BUILDS
+mkdir -p BUILDS/$1
+rm -rf BUILDS/$1/*
+
+gox -os="linux darwin freebsd windows" -output="./BUILDS/$1/centrifugo-$1-{{.OS}}_{{.Arch}}/centrifugo"
+
+
+cd BUILDS/$1
+
+for i in */; do
+  zip -r "${i%/}.zip" "$i"
+  rm -r $i
+done
