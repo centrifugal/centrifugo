4ac5b8e2537066d2641bccb4fe3bb0075dbe1c6c
Alexandr Emelin <frvzmb@gmail.com>
2015-06-16 15:51:28 +0300

    update rpm build script

--- extras/rpm/build.sh
+++ extras/rpm/build.sh
@@ -5,13 +5,14 @@ then
   exit 1
 fi
 
-if ![ -f centrifugo-$1-linux-amd64.zip ]; then
+if [ ! -f centrifugo-$1-linux-amd64.zip ]; then
 	echo "No Centrifugo release found in current directory"
 	exit 1
 fi
 
-git clone https://github.com/centrifugal/centrifuge-web.git ./web
-rm -rf ./web/app/src/
+rm -rf ./centrifuge-web
+git clone https://github.com/centrifugal/centrifuge-web.git ./centrifuge-web
+rm -rf ./centrifuge-web/app/src/
 
 cp centrifugo-$1-linux-amd64.zip ~/rpmbuild/SOURCES/centrifugo-$1-linux-amd64.zip
 cp centrifugo.spec ~/rpmbuild/SPECS/centrifugo.spec
@@ -19,6 +20,7 @@ cp centrifugo.initd ~/rpmbuild/SOURCES/centrifugo.initd
 cp centrifugo.nofiles.conf ~/rpmbuild/SOURCES/centrifugo.nofiles.conf
 cp centrifugo.logrotate ~/rpmbuild/SOURCES/centrifugo.logrotate
 cp centrifugo.config.json ~/rpmbuild/SOURCES/centrifugo.config.json
-cp -rf ./web/app ~/rpmbuild/SOURCES/web
+cp -rf ./centrifuge-web/app ~/rpmbuild/SOURCES/web
+rm -rf ./centrifuge-web
 
 rpmbuild -bb ~/rpmbuild/SPECS/centrifugo.spec --define "version $1" --define "release `date +%s`"
