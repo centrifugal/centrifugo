f8c3ac3be334cc66289e290feb5b4dc5fc44fde1
Alexandr Emelin <frvzmb@gmail.com>
2015-11-25 17:08:40 +0300

    simple test for message

---
+++ libcentrifugo/message_test.go
@@ -0,0 +1,22 @@
+package libcentrifugo
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+
+	"github.com/centrifugal/centrifugo/Godeps/_workspace/src/github.com/stretchr/testify/assert"
+)
+
+func TestMessage(t *testing.T) {
+	msg, err := newMessage(Channel("test"), []byte("{}"), "", nil)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, msg.Channel, Channel("test"))
+	msgBytes, err := json.Marshal(msg)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, strings.Contains(string(msgBytes), "\"channel\":\"test\""))
+	assert.Equal(t, true, strings.Contains(string(msgBytes), "\"data\":{}"))
+	assert.Equal(t, true, strings.Contains(string(msgBytes), "\"client\":\"\""))
+	assert.Equal(t, true, strings.Contains(string(msgBytes), "\"timestamp\":"))
+	assert.Equal(t, true, strings.Contains(string(msgBytes), "\"uid\":"))
+}
