52280dcff71a0b01d8370f3b9d294c7f765cbc4d
FZambia <frvzmb@gmail.com>
2016-02-14 13:40:20 +0300

    release platforms

--- extras/scripts/release.sh
+++ extras/scripts/release.sh
@@ -23,7 +23,7 @@ mkdir -p BUILDS
 mkdir -p BUILDS/$1
 rm -rf BUILDS/$1/*
 
-gox -os="linux" -output="./BUILDS/$1/centrifugo-$1-{{.OS}}-{{.Arch}}/centrifugo"
+gox -os="linux darwin freebsd windows" -output="./BUILDS/$1/centrifugo-$1-{{.OS}}-{{.Arch}}/centrifugo"
 
 cd BUILDS/$1
 
