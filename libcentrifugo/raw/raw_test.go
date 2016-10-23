edd7c85cad69792abf51c419a0ba277217518439
FZambia <frvzmb@gmail.com>
2016-10-23 14:14:12 +0300

    do not test jsonpb equality

---
+++ libcentrifugo/raw/raw_test.go
@@ -0,0 +1,35 @@
+package raw
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+type DataRawMessage struct {
+	Data *json.RawMessage
+}
+
+type DataRaw struct {
+	Data *Raw
+}
+
+func TestRaw(t *testing.T) {
+	data1 := json.RawMessage([]byte(`{"key": "value"}`))
+	stdjsondata1, err := json.Marshal(DataRawMessage{
+		Data: &data1,
+	})
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	data2 := Raw([]byte(`{"key": "value"}`))
+	stdjsondata2, err := json.Marshal(DataRaw{
+		Data: &data2,
+	})
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	if string(stdjsondata1) != string(stdjsondata2) {
+		t.Fatalf("no match", err)
+	}
+}
