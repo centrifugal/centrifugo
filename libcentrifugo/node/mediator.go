7669090fd0162bb85c91d2b7b9f0e0e57b90c3d0
Alexander Emelin <frvzmb@gmail.com>
2017-05-11 09:37:11 +0300

    fix names in comment

--- libcentrifugo/node/mediator.go
+++ libcentrifugo/node/mediator.go
@@ -5,8 +5,7 @@ import (
 )
 
 // Mediator is an interface to work with Centrifugo events from
-// Go code. Implemented Mediator must be set to Application via
-// corresponding Application method SetMediator.
+// Go code. Implemented Mediator must be set to Node on start.
 type Mediator interface {
 	Connect(client string, user string) bool
 	Subscribe(ch string, client string, user string) bool
