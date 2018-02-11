479e2187f1a5813c2081cd01ed0a07f608c24acd
Alexander Emelin <frvzmb@gmail.com>
2018-02-11 03:33:57 +0300

    fix metric names

--- lib/node/metrics.go
+++ lib/node/metrics.go
@@ -11,35 +11,35 @@ var (
 	messagesSentCount = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: metricsNamespace,
 		Subsystem: metricsSubsystem,
-		Name:      "node_messages_sent_count",
+		Name:      "messages_sent_count",
 		Help:      "TODO.",
 	}, []string{"type"})
 
 	messagesReceivedCount = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: metricsNamespace,
 		Subsystem: metricsSubsystem,
-		Name:      "node_messages_received_count",
+		Name:      "messages_received_count",
 		Help:      "TODO.",
 	}, []string{"type"})
 
 	actionCount = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: metricsNamespace,
 		Subsystem: metricsSubsystem,
-		Name:      "node_action_count",
+		Name:      "action_count",
 		Help:      "TODO.",
 	}, []string{"action"})
 
 	numClientsGauge = prometheus.NewGauge(prometheus.GaugeOpts{
 		Namespace: metricsNamespace,
 		Subsystem: metricsSubsystem,
-		Name:      "node_num_clients",
+		Name:      "num_clients",
 		Help:      "TODO.",
 	})
 
 	numUsersGauge = prometheus.NewGauge(prometheus.GaugeOpts{
 		Namespace: metricsNamespace,
 		Subsystem: metricsSubsystem,
-		Name:      "node_num_users",
+		Name:      "num_users",
 		Help:      "TODO.",
 	})
 
