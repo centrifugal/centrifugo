9887b5b5d16b642dcce7d5a603d10fb6fb1aa079
Alexander Emelin <frvzmb@gmail.com>
2018-02-14 21:21:37 +0300

    change proto types, generate proto files from templates

--- extras/proto/generate.sh
+++ extras/proto/generate.sh
@@ -1,33 +1,6 @@
-Install `gomplate`:
+#!/bin/bash
 
-```
-go get github.com/hairyhenderson/gomplate
-```
-
-### API proto
-
-Generate for cross-language API client usage:
-
-```
 gomplate -f $GOPATH/src/github.com/centrifugal/centrifugo/extras/proto/api.template.proto > $GOPATH/src/github.com/centrifugal/centrifugo/extras/proto/api.proto
-```
-
-Generate for internal Centrifugo usage.
-
-```
 GOGO=1 gomplate -f $GOPATH/src/github.com/centrifugal/centrifugo/extras/proto/api.template.proto > $GOPATH/src/github.com/centrifugal/centrifugo/lib/proto/apiproto/api.proto
-```
-
-### Client proto
-
-Generate for cross-language client usage:
-
-```
 gomplate -f $GOPATH/src/github.com/centrifugal/centrifugo/extras/proto/client.template.proto > $GOPATH/src/github.com/centrifugal/centrifugo/extras/proto/client.proto
-```
-
-Generate for internal Centrifugo usage.
-
-```
 GOGO=1 gomplate -f $GOPATH/src/github.com/centrifugal/centrifugo/extras/proto/client.template.proto > $GOPATH/src/github.com/centrifugal/centrifugo/lib/proto/client.proto
-```
