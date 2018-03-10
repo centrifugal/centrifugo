62e48730017e9dab836bd556abfdc921a6a8d054
Alexander Emelin <frvzmb@gmail.com>
2018-03-10 14:09:54 +0300

    v1.7.7

--- centrifugo.rb
+++ centrifugo.rb
@@ -1,8 +1,8 @@
 class Centrifugo < Formula
   desc "Centrifugo"
   homepage "https://github.com/centrifugal/centrifugo"
-  url "https://github.com/centrifugal/centrifugo/releases/download/v1.7.6/centrifugo-1.7.6-darwin-amd64.zip"
-  sha256 "9ab90cd6f6255a9b1f96e162cf6f29d583e921d2afd2067350417fc4e9992deb"
+  url "https://github.com/centrifugal/centrifugo/releases/download/v1.7.7/centrifugo-1.7.7-darwin-amd64.zip"
+  sha256 "f8a09dc6be7fc1bf64e4445d4c93983f0484f8574159f45a0effaf38aeacef94"
 
   def install
     system "chmod", "+x", "centrifugo"
