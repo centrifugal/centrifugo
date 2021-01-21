af9813ef694649b6af06fb1b9784d3649ce1fee3
alexshavelev44 <45029602+alexshavelev44@users.noreply.github.com>
2021-01-21 22:19:26 +0200

    ECDSA JWT algorithm support (#420)
    Co-authored-by: alexshavelev92 <nkuil9sdq>

--- internal/jwtutils/jwt.go
+++ internal/jwtutils/jwt.go
@@ -1,6 +1,7 @@
 package jwtutils
 
 import (
+	"crypto/ecdsa"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
@@ -10,6 +11,7 @@ import (
 var (
 	errKeyMustBePEMEncoded = errors.New("key must be PEM encoded")
 	errNotRSAPublicKey     = errors.New("key is not a valid RSA public key")
+	errNotECDSAPublicKey     = errors.New("key is not a valid ECDSA public key")
 )
 
 // ParseRSAPublicKeyFromPEM parses PEM encoded PKCS1 or PKCS8 public key.
@@ -34,6 +36,33 @@ func ParseRSAPublicKeyFromPEM(key []byte) (*rsa.PublicKey, error) {
 	var ok bool
 	if pkey, ok = parsedKey.(*rsa.PublicKey); !ok {
 		return nil, errNotRSAPublicKey
+	}
+
+	return pkey, nil
+}
+
+// ParseECDSAPublicKeyFromPEM parses PEM encoded public key.
+func ParseECDSAPublicKeyFromPEM(key []byte) (*ecdsa.PublicKey, error) {
+	var err error
+
+	var block *pem.Block
+	if block, _ = pem.Decode(key); block == nil {
+		return nil, errKeyMustBePEMEncoded
+	}
+
+	var parsedKey interface{}
+	if parsedKey, err = x509.ParsePKIXPublicKey(block.Bytes); err != nil {
+		if cert, err := x509.ParseCertificate(block.Bytes); err == nil {
+			parsedKey = cert.PublicKey
+		} else {
+			return nil, err
+		}
+	}
+
+	var pkey *ecdsa.PublicKey
+	var ok bool
+	if pkey, ok = parsedKey.(*ecdsa.PublicKey); !ok {
+		return nil, errNotECDSAPublicKey
 	}
 
 	return pkey, nil
