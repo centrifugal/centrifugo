a6938a269547405b72f4b29ff9f6c17481261614
silischev <a.silischev@yandex.ru>
2021-09-08 11:39:58 +0300

    add joint testing for http and grpc rpc handler

--- internal/proxy/rpc_handler_test.go
+++ internal/proxy/rpc_handler_test.go
@@ -5,33 +5,69 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
-	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/centrifugal/centrifugo/v3/internal/proxyproto"
+
 	"github.com/centrifugal/centrifugo/v3/internal/tools"
 
 	"github.com/centrifugal/centrifuge"
 	"github.com/stretchr/testify/require"
 )
 
-type rpcHandlerTestDepsConfig struct {
+type grpcRPCHandleTestCase struct {
+	*tools.CommonGRPCProxyTestCase
 	rpcProxyHandler *RPCHandler
-	rpcCEvent       centrifuge.RPCEvent
-	transport       *tools.TestTransport
 }
 
-func newRPCHandlerTestDepsConfig(proxyEndpoint string) rpcHandlerTestDepsConfig {
+func newRPCHandlerGRPCTestCase(ctx context.Context, proxyGRPCServer proxyGRPCTestServer) grpcRPCHandleTestCase {
+	commonProxyTestCase := tools.NewCommonGRPCProxyTestCase(ctx, proxyGRPCServer)
+
+	proxyCfg := Config{
+		RPCTimeout: 5 * time.Second,
+		GRPCConfig: GRPCConfig{
+			testDialer: func(ctx context.Context, s string) (net.Conn, error) {
+				return commonProxyTestCase.Listener.Dial()
+			},
+		},
+	}
+
+	rpcProxy, err := NewGRPCRPCProxy(
+		commonProxyTestCase.Listener.Addr().String(),
+		proxyCfg,
+	)
+	if err != nil {
+		log.Fatalln("could not create grpc rpc proxy: ", err)
+	}
+
+	rpcProxyHandler := NewRPCHandler(RPCHandlerConfig{
+		Proxy: rpcProxy,
+	})
+
+	return grpcRPCHandleTestCase{commonProxyTestCase, rpcProxyHandler}
+}
+
+type httpRPCHandleTestCase struct {
+	*tools.CommonHTTPProxyTestCase
+	rpcProxyHandler *RPCHandler
+}
+
+func newRPCHandlerHTTPTestCase(ctx context.Context, proxyEndpoint string) httpRPCHandleTestCase {
+	commonProxyTestCase := tools.NewCommonHTTPProxyTestCase(ctx)
+
 	proxyCfg := Config{
 		HTTPConfig: HTTPConfig{
 			Encoder: &proxyproto.JSONEncoder{},
 			Decoder: &proxyproto.JSONDecoder{},
 		},
+		RPCEndpoint: proxyEndpoint,
 	}
 
 	rpcProxy, err := NewHTTPRPCProxy(
-		proxyEndpoint,
+		commonProxyTestCase.Server.URL+proxyEndpoint,
 		proxyCfg,
 	)
 	if err != nil {
@@ -42,180 +78,189 @@ func newRPCHandlerTestDepsConfig(proxyEndpoint string) rpcHandlerTestDepsConfig 
 		Proxy: rpcProxy,
 	})
 
-	return rpcHandlerTestDepsConfig{
-		rpcProxyHandler: rpcProxyHandler,
-		rpcCEvent:       centrifuge.RPCEvent{},
-		transport:       tools.NewTestTransport(),
+	return httpRPCHandleTestCase{commonProxyTestCase, rpcProxyHandler}
+}
+
+type rpcHandleTestCase struct {
+	rpcProxyHandler *RPCHandler
+	protocol        string
+	node            *centrifuge.Node
+	client          *centrifuge.Client
+}
+
+func (c rpcHandleTestCase) invokeHandle() (reply centrifuge.RPCReply, err error) {
+	rpcHandler := c.rpcProxyHandler.Handle(c.node)
+	reply, err = rpcHandler(c.client, centrifuge.RPCEvent{})
+
+	return reply, err
+}
+
+func newRPCHandlerTestCases(httpTestCase httpRPCHandleTestCase, grpcTestCase grpcRPCHandleTestCase) []rpcHandleTestCase {
+	return []rpcHandleTestCase{
+		{
+			rpcProxyHandler: grpcTestCase.rpcProxyHandler,
+			node:            grpcTestCase.Node,
+			client:          grpcTestCase.Client,
+			protocol:        "grpc",
+		},
+		{
+			rpcProxyHandler: httpTestCase.rpcProxyHandler,
+			node:            httpTestCase.Node,
+			client:          httpTestCase.Client,
+			protocol:        "http",
+		},
 	}
 }
 
 func TestHandleRPCWithResult(t *testing.T) {
-	node := tools.NodeWithMemoryEngineNoHandlers()
-	defer func() { _ = node.Shutdown(context.Background()) }()
-
 	clientData := `{"field":"some data"}`
-	mux := http.NewServeMux()
-	mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
+	opts := proxyGRPCTestServerOptions{Data: []byte(clientData)}
+
+	grpcTestCase := newRPCHandlerGRPCTestCase(context.Background(), newProxyGRPCTestServer("result", opts))
+	defer grpcTestCase.Teardown()
+
+	httpTestCase := newRPCHandlerHTTPTestCase(context.Background(), "/rpc")
+	httpTestCase.Mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
 		_, _ = w.Write([]byte(fmt.Sprintf(`{"result": {"data": %s}}`, clientData)))
 	})
-	server := httptest.NewServer(mux)
-	defer server.Close()
+	defer httpTestCase.Teardown()
 
-	testDepsCfg := newRPCHandlerTestDepsConfig(server.URL + "/rpc")
-	rpcHandler := testDepsCfg.rpcProxyHandler.Handle(node)
-
-	client, closeFn, err := centrifuge.NewClient(context.Background(), node, testDepsCfg.transport)
-	require.NoError(t, err)
-	defer func() { _ = closeFn() }()
-
-	reply, err := rpcHandler(client, testDepsCfg.rpcCEvent)
-	require.NoError(t, err)
-	require.Equal(t, []byte(clientData), reply.Data)
+	cases := newRPCHandlerTestCases(httpTestCase, grpcTestCase)
+	for _, c := range cases {
+		reply, err := c.invokeHandle()
+		require.NoError(t, err, c.protocol)
+		require.Equal(t, []byte(clientData), reply.Data)
+	}
 }
 
 func TestHandleRPCWithContextCancel(t *testing.T) {
-	node := tools.NodeWithMemoryEngineNoHandlers()
-	defer func() { _ = node.Shutdown(context.Background()) }()
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
 
 	clientData := `{"field":"some data"}`
-	mux := http.NewServeMux()
-	mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
+	opts := proxyGRPCTestServerOptions{Data: []byte(clientData)}
+
+	grpcTestCase := newRPCHandlerGRPCTestCase(ctx, newProxyGRPCTestServer("result", opts))
+	defer grpcTestCase.Teardown()
+
+	httpTestCase := newRPCHandlerHTTPTestCase(ctx, "/rpc")
+	httpTestCase.Mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
 		_, _ = w.Write([]byte(fmt.Sprintf(`{"result": {"data": %s}}`, clientData)))
 	})
-	server := httptest.NewServer(mux)
-	defer server.Close()
+	defer httpTestCase.Teardown()
 
-	testDepsCfg := newRPCHandlerTestDepsConfig(server.URL + "/rpc")
-	rpcHandler := testDepsCfg.rpcProxyHandler.Handle(node)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	client, closeFn, err := centrifuge.NewClient(ctx, node, testDepsCfg.transport)
-	require.NoError(t, err)
-	defer func() { _ = closeFn() }()
-
-	cancel()
-	reply, err := rpcHandler(client, testDepsCfg.rpcCEvent)
-	require.ErrorIs(t, centrifuge.DisconnectNormal, err)
-	require.Equal(t, centrifuge.RPCReply{}, reply)
+	cases := newRPCHandlerTestCases(httpTestCase, grpcTestCase)
+	for _, c := range cases {
+		reply, err := c.invokeHandle()
+		require.ErrorIs(t, centrifuge.DisconnectNormal, err, c.protocol)
+		require.Equal(t, centrifuge.RPCReply{}, reply, c.protocol)
+	}
 }
 
 func TestHandleRPCWithoutProxyServerStart(t *testing.T) {
-	node := tools.NodeWithMemoryEngineNoHandlers()
-	defer func() { _ = node.Shutdown(context.Background()) }()
+	grpcTestCase := newRPCHandlerGRPCTestCase(context.Background(), proxyGRPCTestServer{})
+	grpcTestCase.Teardown()
 
-	testDepsCfg := newRPCHandlerTestDepsConfig("/rpc")
-	rpcHandler := testDepsCfg.rpcProxyHandler.Handle(node)
+	httpTestCase := newRPCHandlerHTTPTestCase(context.Background(), "/rpc")
+	httpTestCase.Teardown()
 
-	client, closeFn, err := centrifuge.NewClient(context.Background(), node, testDepsCfg.transport)
-	require.NoError(t, err)
-	defer func() { _ = closeFn() }()
+	cases := newRPCHandlerTestCases(httpTestCase, grpcTestCase)
+	for _, c := range cases {
+		reply, err := c.invokeHandle()
+		require.ErrorIs(t, centrifuge.ErrorInternal, err)
+		require.Equal(t, centrifuge.RPCReply{}, reply)
+	}
 
-	reply, err := rpcHandler(client, testDepsCfg.rpcCEvent)
-	require.ErrorIs(t, centrifuge.ErrorInternal, err)
-	require.Equal(t, centrifuge.RPCReply{}, reply)
 }
 
 func TestHandleRPCWithProxyServerCustomDisconnect(t *testing.T) {
-	node := tools.NodeWithMemoryEngineNoHandlers()
-	defer func() { _ = node.Shutdown(context.Background()) }()
+	grpcTestCase := newRPCHandlerGRPCTestCase(context.Background(), newProxyGRPCTestServer("custom disconnect", proxyGRPCTestServerOptions{}))
+	defer grpcTestCase.Teardown()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
+	httpTestCase := newRPCHandlerHTTPTestCase(context.Background(), "/rpc")
+	httpTestCase.Mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
 		_, _ = w.Write([]byte(`{"disconnect": {"code": 4000, "reconnect": false, "reason": "custom disconnect"}}`))
 	})
-	server := httptest.NewServer(mux)
-	defer server.Close()
+	defer httpTestCase.Teardown()
 
-	testDepsCfg := newRPCHandlerTestDepsConfig(server.URL + "/rpc")
-	rpcHandler := testDepsCfg.rpcProxyHandler.Handle(node)
-
-	client, closeFn, err := centrifuge.NewClient(context.Background(), node, testDepsCfg.transport)
-	require.NoError(t, err)
-	defer func() { _ = closeFn() }()
-
-	reply, err := rpcHandler(client, testDepsCfg.rpcCEvent)
 	expectedErr := centrifuge.Disconnect{
 		Code:      4000,
 		Reason:    "custom disconnect",
 		Reconnect: false,
 	}
-	require.NotNil(t, err)
-	require.Equal(t, expectedErr.Error(), err.Error())
-	require.Equal(t, centrifuge.RPCReply{}, reply)
+
+	cases := newRPCHandlerTestCases(httpTestCase, grpcTestCase)
+	for _, c := range cases {
+		reply, err := c.invokeHandle()
+		require.NotNil(t, err, c.protocol)
+		require.Equal(t, expectedErr.Error(), err.Error(), c.protocol)
+		require.Equal(t, centrifuge.RPCReply{}, reply, c.protocol)
+	}
 }
 
 func TestHandleRPCWithProxyServerCustomError(t *testing.T) {
-	node := tools.NodeWithMemoryEngineNoHandlers()
-	defer func() { _ = node.Shutdown(context.Background()) }()
+	grpcTestCase := newRPCHandlerGRPCTestCase(context.Background(), newProxyGRPCTestServer("custom error", proxyGRPCTestServerOptions{}))
+	defer grpcTestCase.Teardown()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
+	httpTestCase := newRPCHandlerHTTPTestCase(context.Background(), "/rpc")
+	httpTestCase.Mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
 		_, _ = w.Write([]byte(`{"error": {"code": 1000, "message": "custom error"}}`))
 	})
-	server := httptest.NewServer(mux)
-	defer server.Close()
+	defer httpTestCase.Teardown()
 
-	testDepsCfg := newRPCHandlerTestDepsConfig(server.URL + "/rpc")
-	rpcHandler := testDepsCfg.rpcProxyHandler.Handle(node)
-
-	client, closeFn, err := centrifuge.NewClient(context.Background(), node, testDepsCfg.transport)
-	require.NoError(t, err)
-	defer func() { _ = closeFn() }()
-
-	reply, err := rpcHandler(client, testDepsCfg.rpcCEvent)
 	expectedErr := centrifuge.Error{
 		Code:    1000,
 		Message: "custom error",
 	}
-	require.NotNil(t, err)
-	require.Equal(t, expectedErr.Error(), err.Error())
-	require.Equal(t, centrifuge.RPCReply{}, reply)
+
+	cases := newRPCHandlerTestCases(httpTestCase, grpcTestCase)
+	for _, c := range cases {
+		reply, err := c.invokeHandle()
+		require.NotNil(t, err, c.protocol)
+		require.Equal(t, expectedErr.Error(), err.Error(), c.protocol)
+		require.Equal(t, centrifuge.RPCReply{}, reply, c.protocol)
+	}
 }
 
 func TestHandleRPCWithValidCustomData(t *testing.T) {
-	node := tools.NodeWithMemoryEngineNoHandlers()
-	defer func() { _ = node.Shutdown(context.Background()) }()
-
-	customData := "test"
+	customData := `test`
 	customDataB64 := base64.StdEncoding.EncodeToString([]byte(customData))
+	opts := proxyGRPCTestServerOptions{B64Data: customDataB64}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
+	grpcTestCase := newRPCHandlerGRPCTestCase(context.Background(), newProxyGRPCTestServer("custom data", opts))
+	defer grpcTestCase.Teardown()
+
+	httpTestCase := newRPCHandlerHTTPTestCase(context.Background(), "/rpc")
+	httpTestCase.Mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
 		_, _ = w.Write([]byte(fmt.Sprintf(`{"result": {"b64data": "%s"}}`, customDataB64)))
 	})
-	server := httptest.NewServer(mux)
-	defer server.Close()
+	defer httpTestCase.Teardown()
 
-	testDepsCfg := newRPCHandlerTestDepsConfig(server.URL + "/rpc")
-	rpcHandler := testDepsCfg.rpcProxyHandler.Handle(node)
-
-	client, closeFn, err := centrifuge.NewClient(context.Background(), node, testDepsCfg.transport)
-	require.NoError(t, err)
-	defer func() { _ = closeFn() }()
-
-	reply, err := rpcHandler(client, testDepsCfg.rpcCEvent)
-	require.NoError(t, err)
-	require.Equal(t, []byte(customData), reply.Data)
+	cases := newRPCHandlerTestCases(httpTestCase, grpcTestCase)
+	for _, c := range cases {
+		reply, err := c.invokeHandle()
+		require.NoError(t, err, c.protocol)
+		require.Equal(t, []byte(customData), reply.Data)
+	}
 }
 
 func TestHandleRPCWithInvalidCustomData(t *testing.T) {
-	node := tools.NodeWithMemoryEngineNoHandlers()
-	defer func() { _ = node.Shutdown(context.Background()) }()
-	mux := http.NewServeMux()
-	mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
+	opts := proxyGRPCTestServerOptions{
+		B64Data: "invalid data",
+	}
+	grpcTestCase := newRPCHandlerGRPCTestCase(context.Background(), newProxyGRPCTestServer("custom data", opts))
+	defer grpcTestCase.Teardown()
+
+	httpTestCase := newRPCHandlerHTTPTestCase(context.Background(), "/rpc")
+	httpTestCase.Mux.HandleFunc("/rpc", func(w http.ResponseWriter, req *http.Request) {
 		_, _ = w.Write([]byte(`{"result": {"b64data": "invalid data"}}`))
 	})
-	server := httptest.NewServer(mux)
-	defer server.Close()
+	defer httpTestCase.Teardown()
 
-	testDepsCfg := newRPCHandlerTestDepsConfig(server.URL + "/rpc")
-	rpcHandler := testDepsCfg.rpcProxyHandler.Handle(node)
-
-	client, closeFn, err := centrifuge.NewClient(context.Background(), node, testDepsCfg.transport)
-	require.NoError(t, err)
-	defer func() { _ = closeFn() }()
-
-	reply, err := rpcHandler(client, testDepsCfg.rpcCEvent)
-	require.ErrorIs(t, centrifuge.ErrorInternal, err)
-	require.Equal(t, centrifuge.RPCReply{}, reply)
+	cases := newRPCHandlerTestCases(httpTestCase, grpcTestCase)
+	for _, c := range cases {
+		reply, err := c.invokeHandle()
+		require.ErrorIs(t, centrifuge.ErrorInternal, err, c.protocol)
+		require.Equal(t, centrifuge.RPCReply{}, reply, c.protocol)
+	}
 }
