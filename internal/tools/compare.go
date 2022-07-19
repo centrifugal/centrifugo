c1cc5c3e39e0e3ebeb34650899f259a86a574172
jianghang <jianghang@didiglobal.com>
2022-07-19 20:34:18 +0800

    Lint

--- internal/tools/compare.go
+++ internal/tools/compare.go
@@ -5,10 +5,7 @@ import "crypto/subtle"
 // SecureCompare use constant time function to compare the two given array.
 func SecureCompare(given, actual []byte) bool {
 	if subtle.ConstantTimeEq(int32(len(given)), int32(len(actual))) == 1{
-		if subtle.ConstantTimeCompare(given, actual) == 1{
-			return true
-		}
-		return false
+		return subtle.ConstantTimeCompare(given, actual) == 1
 	}
 	// Securely compare actual to itself to keep constant time, but always return false
 	if subtle.ConstantTimeCompare(actual, actual) == 1 {
@@ -23,10 +20,7 @@ func SecureCompareString(given, actual string) bool {
 	// return SecureCompare([]byte(given), []byte(actual))
 
 	if subtle.ConstantTimeEq(int32(len(given)), int32(len(actual))) == 1 {
-		if subtle.ConstantTimeCompare([]byte(given), []byte(actual)) == 1 {
-			return true
-		}
-		return false
+		return subtle.ConstantTimeCompare([]byte(given), []byte(actual)) == 1
 	}
 	// Securely compare actual to itself to keep constant time, but always return false
 	if subtle.ConstantTimeCompare([]byte(actual), []byte(actual)) == 1 {
