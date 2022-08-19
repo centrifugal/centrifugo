c22e33e8a994257d430fbca09baa6d8e19ba253b
Alexander Emelin <frvzmb@gmail.com>
2022-08-19 11:42:33 +0300

    add prometheus counter

--- internal/middleware/connlimit.go
+++ internal/middleware/connlimit.go
@@ -3,14 +3,31 @@ package middleware
 import (
 	"net/http"
 
-	"github.com/centrifugal/centrifuge"
 	"github.com/centrifugal/centrifugo/v4/internal/rule"
+
+	"github.com/centrifugal/centrifuge"
+	"github.com/prometheus/client_golang/prometheus"
 )
+
+var (
+	connLimitReached prometheus.Counter
+)
+
+func init() {
+	connLimitReached = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "centrifugo",
+		Subsystem: "node",
+		Name:      "client_connection_limit",
+		Help:      "Number of refused requests due to node client connection limit.",
+	})
+	_ = prometheus.DefaultRegisterer.Register(connLimitReached)
+}
 
 func ConnLimit(node *centrifuge.Node, ruleContainer *rule.Container, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		connLimit := ruleContainer.Config().ClientConnectionLimit
 		if connLimit > 0 && node.Hub().NumClients() >= connLimit {
+			connLimitReached.Inc()
 			node.Log(centrifuge.NewLogEntry(centrifuge.LogLevelWarn, "node connection limit reached", map[string]interface{}{"limit": connLimit}))
 			return
 		}
