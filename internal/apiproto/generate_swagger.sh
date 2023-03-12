12e91f26450a4b8e03e80ed87140f833f1ac3da5
Alexander Emelin <frvzmb@gmail.com>
2023-03-12 22:06:48 +0200

    fix json case, fix number types, fix byte types

--- internal/apiproto/generate_swagger.sh
+++ internal/apiproto/generate_swagger.sh
@@ -5,9 +5,9 @@ set -e
 #go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@latest
 #go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
 
-protoc --openapiv2_out=allow_merge=true,merge_file_name=api:. ./api.swagger.proto
+protoc --openapiv2_out=json_names_for_fields=false,allow_merge=true,merge_file_name=api:. ./api.swagger.proto
 
-search='("data":\s+{\s+"type":\s)"string",\s+"format":\s"byte"'
+search='(:\s+{\s+"type":\s)"string",\s+"format":\s"byte"'
 replace='\1"object"'
 
 python - << EOF
@@ -25,6 +25,31 @@ def replace(filePath, text, subs, flags=0):
         file.write(file_contents)
 
     
+file_path="api.swagger.json"
+text=r'$search'
+subs=r'$replace'
+#calling the replace method
+replace(file_path, text, subs, flags=re.MULTILINE | re.DOTALL)
+EOF
+
+search='(:\s+{\s+"type":\s)"string",\s+"format":\s"uint64"'
+replace='\1"integer"'
+
+python - << EOF
+import re
+
+#defining the replace method
+def replace(filePath, text, subs, flags=0):
+    with open(file_path, "r+") as file:
+        #read the file contents
+        file_contents = file.read()
+        text_pattern = re.compile(text, flags)
+        file_contents = text_pattern.sub(subs, file_contents)
+        file.seek(0)
+        file.truncate()
+        file.write(file_contents)
+
+
 file_path="api.swagger.json"
 text=r'$search'
 subs=r'$replace'
