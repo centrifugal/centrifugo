8b5c4b8163920d5b7270f01401e08ee7948c9727
FZambia <frvzmb@gmail.com>
2024-09-23 17:25:09 +0300

    fix lint

--- internal/runutil/engine.go
+++ internal/runutil/engine.go
@@ -1,6 +1,8 @@
 package runutil
 
 import (
+	"fmt"
+
 	"github.com/centrifugal/centrifugo/v5/internal/config"
 	"github.com/centrifugal/centrifugo/v5/internal/confighelpers"
 	"github.com/centrifugal/centrifugo/v5/internal/natsbroker"
@@ -44,15 +46,18 @@ func redisEngine(n *centrifuge.Node, cfgContainer *config.Container) (*centrifug
 	cfg := cfgContainer.Config()
 	redisShards, mode, err := confighelpers.CentrifugeRedisShards(n, cfg.Redis.Redis)
 	if err != nil {
-		return nil, nil, "", err
+		return nil, nil, "", fmt.Errorf("error creating Redis shards: %w", err)
 	}
 	broker, err := confighelpers.CentrifugeRedisBroker(
 		n, cfg.Redis.Prefix, redisShards, cfg.Redis.RedisBrokerCommon, cfg.Broker == "redisnats")
 	if err != nil {
-		return nil, nil, mode, err
+		return nil, nil, mode, fmt.Errorf("error creating Redis broker: %w", err)
 	}
 	presenceManager, err := confighelpers.CentrifugeRedisPresenceManager(
 		n, cfg.Redis.Prefix, redisShards, cfg.Redis.RedisPresenceManagerCommon)
+	if err != nil {
+		return nil, nil, mode, fmt.Errorf("error creating Redis presence manager: %w", err)
+	}
 
 	return broker, presenceManager, mode, nil
 }
