8f76887cceeff313f0c6779288b7f5bdc383a406
Alexander Emelin <frvzmb@gmail.com>
2025-04-14 20:28:04 +0300

    Nats JetStream consumer improvements (#972)

--- internal/configtypes/types.go
+++ internal/configtypes/types.go
@@ -830,13 +830,16 @@ type NatsJetStreamConsumerConfig struct {
 	Password string `mapstructure:"password" json:"password" toml:"password" yaml:"password"`
 	// Token is an alternative authentication mechanism if CredentialsFile and Username are not provided.
 	Token string `mapstructure:"token" json:"token" toml:"token" yaml:"token"`
-	// Subjects is the list of NATS subjects (topics) to subscribe to.
+	// StreamName is the name of the NATS JetStream stream to use.
+	StreamName string `mapstructure:"stream_name" json:"stream_name" toml:"stream_name" yaml:"stream_name"`
+	// Subjects is the list of NATS subjects (topics) to filter.
 	Subjects []string `mapstructure:"subjects" json:"subjects" toml:"subjects" yaml:"subjects"`
 	// DurableConsumerName sets the name of the durable JetStream consumer to use.
 	DurableConsumerName string `mapstructure:"durable_consumer_name" json:"durable_consumer_name" toml:"durable_consumer_name" yaml:"durable_consumer_name"`
-	// Ordered enables JetStream's ordered consumer mode.
-	// In this mode, only one consumer instance receives messages at a time, preserving exact order.
-	Ordered bool `mapstructure:"ordered" json:"ordered" toml:"ordered" yaml:"ordered"`
+	// DeliverPolicy is the NATS JetStream delivery policy for the consumer. By default, it is set to "new". Possible values: `new`, `all`.
+	DeliverPolicy string `mapstructure:"deliver_policy" default:"new" json:"deliver_policy" toml:"deliver_policy" yaml:"deliver_policy"`
+	// MaxAckPending is the maximum number of unacknowledged messages that can be pending for the consumer.
+	MaxAckPending int `mapstructure:"max_ack_pending" default:"100" json:"max_ack_pending" toml:"max_ack_pending" yaml:"max_ack_pending"`
 	// MethodHeader is the NATS message header used to extract the method name for dispatching commands.
 	MethodHeader string `mapstructure:"method_header" json:"method_header" toml:"method_header" yaml:"method_header"`
 	// PublicationDataMode configures extraction of pre-formatted publication data from message headers.
@@ -868,11 +871,14 @@ func (cfg NatsJetStreamConsumerConfig) Validate() error {
 	if cfg.URL == "" {
 		return errors.New("url is required")
 	}
-	if len(cfg.Subjects) == 0 {
-		return errors.New("subjects can't be empty")
+	if cfg.StreamName == "" {
+		return errors.New("stream_name is required")
 	}
 	if cfg.DurableConsumerName == "" {
-		return errors.New("durable is required")
+		return errors.New("durable_consumer_name is required for consumer")
+	}
+	if cfg.DeliverPolicy != "new" && cfg.DeliverPolicy != "all" {
+		return errors.New("deliver_policy must be either 'new' or 'all'")
 	}
 	if cfg.PublicationDataMode.Enabled && cfg.PublicationDataMode.ChannelsHeader == "" {
 		return errors.New("channels_header is required for publication data mode")
