ebe4125dea9fed528251fe7d4b106ce5ad9ed65b
FZambia <frvzmb@gmail.com>
2025-10-28 20:01:42 +0200

    write deadline for uni ws

--- internal/uniws/transport.go
+++ internal/uniws/transport.go
@@ -139,6 +139,18 @@ func (t *websocketTransport) writeData(data []byte) error {
 	}
 	if t.opts.writeTimeout > 0 {
 		_ = t.conn.SetWriteDeadline(time.Time{})
+		if t.opts.protoMajor > 1 {
+			// For HTTP/2 connections, we need to actually clear the deadline on the underlying
+			// connection. The websocket Conn.SetWriteDeadline only sets a field, but doesn't
+			// clear the deadline that was already set on the underlying net.Conn during write.
+			// This is critical for HTTP/2 ResponseController where expired deadlines cannot be
+			// extended and will cause the stream to fail permanently.
+			err = t.conn.NetConn().SetWriteDeadline(time.Time{})
+			if err != nil {
+				t.writeMu.Unlock()
+				return err
+			}
+		}
 	}
 	t.writeMu.Unlock()
 
