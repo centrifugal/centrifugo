1879c183cc589b0ff08412bed42e42904f7a600e
FZambia <frvzmb@gmail.com>
2026-01-01 10:45:43 +0200

    fix tests

--- internal/consuming/consuming_test.go
+++ internal/consuming/consuming_test.go
@@ -3,10 +3,24 @@
 package consuming
 
 import (
+	"os"
+	"testing"
+
+	"github.com/centrifugal/centrifugo/v6/internal/metrics"
+
 	"github.com/google/uuid"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/rs/zerolog/log"
 )
+
+func TestMain(m *testing.M) {
+	// Initialize metrics with a custom registry for tests to avoid conflicts
+	registry := prometheus.NewRegistry()
+	_ = metrics.Init(metrics.Config{
+		Registerer: registry,
+	})
+	os.Exit(m.Run())
+}
 
 func testCommon(registry prometheus.Registerer) *consumerCommon {
 	return &consumerCommon{
