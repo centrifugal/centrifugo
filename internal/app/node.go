593ecd4e549b60f201f8b2ff7935923e41a93d05
FZambia <frvzmb@gmail.com>
2026-03-15 19:28:57 +0200

    fix startup

--- internal/app/node.go
+++ internal/app/node.go
@@ -66,24 +66,35 @@ func centrifugeNodeConfig(version string, edition string, cfgContainer *config.C
 			MetaTTL:       chOpts.MapMetaTTL.ToDuration(),
 		}
 	}
-	cfg.GetSharedPollChannelOptions = func(channel string) (centrifuge.SharedPollChannelOptions, bool) {
-		_, _, chOpts, ok, err := cfgContainer.ChannelOptions(channel)
-		if err != nil || !ok {
-			return centrifuge.SharedPollChannelOptions{}, false
+	hasSharedPoll := appCfg.Channel.WithoutNamespace.SubscriptionType == "shared_poll"
+	if !hasSharedPoll {
+		for _, ns := range appCfg.Channel.Namespaces {
+			if ns.SubscriptionType == "shared_poll" {
+				hasSharedPoll = true
+				break
+			}
 		}
-		if chOpts.SubscriptionType != "shared_poll" {
-			return centrifuge.SharedPollChannelOptions{}, false
+	}
+	if hasSharedPoll {
+		cfg.GetSharedPollChannelOptions = func(channel string) (centrifuge.SharedPollChannelOptions, bool) {
+			_, _, chOpts, ok, err := cfgContainer.ChannelOptions(channel)
+			if err != nil || !ok {
+				return centrifuge.SharedPollChannelOptions{}, false
+			}
+			if chOpts.SubscriptionType != "shared_poll" {
+				return centrifuge.SharedPollChannelOptions{}, false
+			}
+			return centrifuge.SharedPollChannelOptions{
+				MaxTrackedPerConnection:        chOpts.SharedPoll.MaxTrackedPerConnection,
+				RefreshInterval:                chOpts.SharedPoll.RefreshInterval.ToDuration(),
+				RefreshBatchSize:               chOpts.SharedPoll.RefreshBatchSize,
+				RefreshConcurrency:             chOpts.SharedPoll.RefreshConcurrency,
+				SendItemVersions:               chOpts.SharedPoll.SendItemVersions,
+				KeepLatestData:                 chOpts.SharedPoll.KeepLatestData,
+				MaxConsecutiveAbsences:         chOpts.SharedPoll.MaxConsecutiveAbsences,
+				SharedPollChannelShutdownDelay: chOpts.SharedPoll.SharedPollChannelShutdownDelay.ToDuration(),
+			}, true
 		}
-		return centrifuge.SharedPollChannelOptions{
-			MaxTrackedPerConnection:        chOpts.SharedPoll.MaxTrackedPerConnection,
-			RefreshInterval:                chOpts.SharedPoll.RefreshInterval.ToDuration(),
-			RefreshBatchSize:               chOpts.SharedPoll.RefreshBatchSize,
-			RefreshConcurrency:             chOpts.SharedPoll.RefreshConcurrency,
-			SendItemVersions:               chOpts.SharedPoll.SendItemVersions,
-			KeepLatestData:                 chOpts.SharedPoll.KeepLatestData,
-			MaxConsecutiveAbsences:         chOpts.SharedPoll.MaxConsecutiveAbsences,
-			SharedPollChannelShutdownDelay: chOpts.SharedPoll.SharedPollChannelShutdownDelay.ToDuration(),
-		}, true
 	}
 	cfg.HistoryMetaTTL = appCfg.Channel.HistoryMetaTTL.ToDuration()
 	cfg.ClientConnectIncludeServerTime = appCfg.Client.ConnectIncludeServerTime
