200fba6bd20765dbaa74700a392336205795321d
FZambia <frvzmb@gmail.com>
2026-03-19 21:23:17 +0200

    grpc shared poll proxy

--- internal/proxy/shared_poll_refresh_grpc.go
+++ internal/proxy/shared_poll_refresh_grpc.go
@@ -5,23 +5,43 @@ import (
 	"fmt"
 
 	"github.com/centrifugal/centrifugo/v6/internal/proxyproto"
+
+	"google.golang.org/grpc"
 )
 
 // GRPCSharedPollRefreshProxy ...
 type GRPCSharedPollRefreshProxy struct {
 	config Config
+	client proxyproto.CentrifugoProxyClient
 }
 
 var _ SharedPollRefreshProxy = (*GRPCSharedPollRefreshProxy)(nil)
 
 // NewGRPCSharedPollRefreshProxy ...
-func NewGRPCSharedPollRefreshProxy(_ string, _ Config) (*GRPCSharedPollRefreshProxy, error) {
-	return nil, fmt.Errorf("gRPC shared poll refresh proxy is not supported yet, use HTTP endpoint")
+func NewGRPCSharedPollRefreshProxy(name string, p Config) (*GRPCSharedPollRefreshProxy, error) {
+	host, err := getGrpcHost(p.Endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("error getting grpc host: %v", err)
+	}
+	dialOpts, err := getDialOpts(name, p)
+	if err != nil {
+		return nil, fmt.Errorf("error creating GRPC dial options: %v", err)
+	}
+	conn, err := grpc.NewClient(host, dialOpts...)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to GRPC proxy server: %v", err)
+	}
+	return &GRPCSharedPollRefreshProxy{
+		config: p,
+		client: proxyproto.NewCentrifugoProxyClient(conn),
+	}, nil
 }
 
-// ProxySharedPollRefresh ...
-func (p *GRPCSharedPollRefreshProxy) ProxySharedPollRefresh(_ context.Context, _ *proxyproto.SharedPollRefreshRequest) (*proxyproto.SharedPollRefreshResponse, error) {
-	return nil, fmt.Errorf("not implemented")
+// ProxySharedPollRefresh proxies SharedPollRefresh to application backend.
+func (p *GRPCSharedPollRefreshProxy) ProxySharedPollRefresh(ctx context.Context, req *proxyproto.SharedPollRefreshRequest) (*proxyproto.SharedPollRefreshResponse, error) {
+	ctx, cancel := context.WithTimeout(ctx, p.config.Timeout.ToDuration())
+	defer cancel()
+	return p.client.SharedPollRefresh(grpcRequestContext(ctx, p.config), req)
 }
 
 // Protocol ...
